4acd67ace15a31643586c6a75f6b494144df4e4b
Mohd Aamil <123925807+mohdaamil120@users.noreply.github.com>
2023-12-11 16:41:44 +0530

    frontend part

---
+++ src/components/MainRoutes.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+export default function MainRoutes() {
+  return (
+    <div>
+        <Routes>
+            <Route path='/'/>
+        </Routes>
+    </div>
+  )
+}
