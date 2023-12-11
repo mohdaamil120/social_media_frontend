0d853d3beebab9a6c657d8837873a2881ecc2f31
Mohd Aamil <123925807+mohdaamil120@users.noreply.github.com>
2023-12-11 16:48:56 +0530

    navbar done

--- src/components/MainRoutes.jsx
+++ src/components/MainRoutes.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import Signup from './Signup'
+import Login from './Login'
+import CreatePost from './CreatePost'
+import Posts from './Posts'
 
 export default function MainRoutes() {
   return (
     <div>
         <Routes>
-            <Route path='/'/>
+            <Route path='/register' element={<Signup/>}/>
+            <Route path='/login' element={<Login/>}/>
+            <Route path='/add' element={<CreatePost/>}/>
+            <Route path='/posts' element={<Posts/>}/>
         </Routes>
     </div>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <div>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/register"}>Register</Link>
+        <Link to={"/login"}>Login</Link>
+        <Link to={"/add"}>Add Post</Link>
+        
+    </div>
   )
 }
